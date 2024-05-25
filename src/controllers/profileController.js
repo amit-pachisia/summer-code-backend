0e5af6e643761fbd5285423c60cb3df32899bfe7
dasanub08 <anubrata.life08@gmail.com>
2024-05-25 20:42:20 +0530

    added create controller for profile

---
+++ src/controllers/profileController.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Profile = mongoose.model('Profile');
+
+
+/**
+ * Create the profile json
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {Object}
+ */
+const createProfile = async (req, res) => {
+    try {
+        const payload = req.body;
+        const instance = new Profile(payload);
+        await instance.save();
+        res.json({ success: true });
+    } catch ({ message }) {
+        res.send({ error: true, message });
+    }
+};
