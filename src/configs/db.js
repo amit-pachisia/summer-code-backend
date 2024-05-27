67f9ecb3ce61eccf80e8915cbcc6678333327d27
karananeja <karananeja.r@gmail.com>
2024-05-27 23:29:02 +0530

    connecting to the db on server start and added an extra route to prevent random route access

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async (DB_URI) => {
   try {
     await mongoose.connect(DB_URI); // Database connection 🥳
-    console.log("Connection to Database Established");
+    console.log('[database] Connection to Database Established');
   } catch (err) {
-    console.log("Could not connect to MongoDB");
+    console.log('Could not connect to MongoDB');
     console.log(err);
     process.exit(1);
   }
