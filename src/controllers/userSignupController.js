7b4f24ce3af8bf2e92aa6b72409ef7c5b1c0288e
Suvrata Dwivedi <=suvratadwivedi@gmail.com>
2024-05-27 22:40:12 +0530

    Modified sign up route

--- src/controllers/userSignupController.js
+++ src/controllers/userSignupController.js
@@ -1,61 +1,28 @@
-const fs = require('fs');
-const path = require('path');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const SAMPLE_USER_DATA = require('../data/sampleUser');
+const User = require('../models/userModel');
+const hashUtils = require('../utils/hashUtils');
 
-const usersFilePath = path.join(__dirname, '../data/users.json');
-
-const register = (req, res) => {
-    const { email, password } = req.body;
-
-    fs.exists(usersFilePath, (exists) => {
-        if (!exists) {
-            fs.writeFile(usersFilePath, '[]', (err) => {
-                if (err) {
-                    console.error('Error creating users.json file:', err);
-                    return res.status(500).send({ message: 'Internal server error' });
-                }
-                console.log('users.json file created');
-                addUser(email, password, res);
-            });
-        } else {
-            addUser(email, password, res);
+const register = async (req,res) =>{
+     const {firstname, lastname,email, password} = req.body;
+     try{
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.status(400).json({message: 'User already exists'});
         }
-    });
-};
-
-const addUser = (email, password, res) => {
-    fs.readFile(usersFilePath, (err, data) => {
-        if (err) {
-            console.error('Error reading users.json file:', err);
-            return res.status(500).send({ message: 'Internal server error' });
-        }
-
-        const users = JSON.parse(data);
-        const existingUser = users.find(user => user.email === email);
-        if (existingUser) {
-            return res.status(400).send({ message: 'User already exists' });
-        }
-
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        const newUser = { id: users.length + 1, email, password: hashedPassword, ...SAMPLE_USER_DATA };
-
-        users.push(newUser);
-
-        fs.writeFile(usersFilePath, JSON.stringify(users), (err) => {
-            if (err) {
-                console.error('Error writing to users.json file:', err);
-                return res.status(500).send({ message: 'Internal server error' });
-            }
-
-            const token = jwt.sign({ id: newUser.id, email: newUser.email }, process.env.JWT_SECRET, {
-                expiresIn: '1h',
-            });
-            res.status(200).send({ message: 'User registered successfully' ,token});
+        let hashedPassword = await hashUtils.hashPassword(password);
+        const user = new User({firstname,lastname, email, password: hashedPassword});
+        await user.save();
+        const token = jwt.sign({id: user.id, email:user.email},process.env.JWT_SECRET,{
+            expiresIn: '30d'
         });
-    });
-};
+        res.status(200).json({message: 'User registered successfully',token,firstname,lastname,email});
+
+     }
+     catch(err){
+        res.status(500).json({message: 'Server Error'});
+     }
+}
+
 
 module.exports = {
     register,
